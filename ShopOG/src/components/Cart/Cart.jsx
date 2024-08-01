61cf0434dcbb5c97964a9a0e6e9d56117a4925e1
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-08-01 21:42:39 +0100

    ShopOG

--- ShopOG/src/components/Cart/Cart.jsx
+++ ShopOG/src/components/Cart/Cart.jsx
@@ -12,6 +12,7 @@ const Cart = ({ openModal, setOpen }) => {
     const cart = useSelector(state => state.cart.cartList)
     const totalAmount = useSelector(state => state.cart.totalAmount)
     const totalPrice = useSelector(state => state.cart.totalPrice)
+    const oneAmount = useSelector(state => state.cart.amount)
     console.log(cart)
     console.log(totalAmount)
     console.log(totalPrice)
@@ -23,20 +24,27 @@ const Cart = ({ openModal, setOpen }) => {
             <Dialog
                 onClick={e => e.stopPropagation()}
                 open={openModal}
+                
                 handler={() => setOpen(false)}
                 animate={{
                     mount: { scale: 1, y: 0 },
                     unmount: { scale: 0.9, y: -100 },
                 }}
             >
+                <DialogHeader>Shopping Cart</DialogHeader>
                 {cart.length > 0 ? cart.map(item => (
                     <div key={item.id}>
-                        <DialogHeader>
+
+                        <DialogBody className="flex justify-start items-center gap-5">
                             <img src={item.img} className="w-16 hover:shadow-md rounded-lg border mx-2 p-1" />
-                            {item.name}
+                            <p className="text-lg font-inter text-black">{item.name}</p>
+                            <div>
                             <span className="mx-2 font-inter text-base text-blue-800">{item.price}$</span>
-                        </DialogHeader>
-                        {/* <DialogBody>{item.price}</DialogBody> */}
+                            <p className="text-sm font-inter text-black">Qte {item.amount}</p>
+                            </div>
+                            <span className="mx-2 font-inter text-base text-blue-800">{item.price * item.amount}$</span>
+                            <Button color="red" className="text-xs right-0">Remove</Button>
+                        </DialogBody>
                     </div>
                 )) : <DialogHeader>No Product Selected</DialogHeader>}
 
