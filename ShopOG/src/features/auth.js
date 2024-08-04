eb4828861e3c44847411a7fbf4a172f49b639366
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-08-04 01:27:05 +0100

    ShopOG

--- ShopOG/src/features/auth.js
+++ ShopOG/src/features/auth.js
@@ -4,13 +4,29 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
     name:'auth',
     initialState: {
-        name:'',
-        password:'',
-        authUser:false
+        user: {
+            name:'',
+            password:'',
+            authUser:false
+        }
     },
     reducers:{
        login:(state,action)=> {
-
+            const userId = action.payload;
+            const userValidation = /^[A-Za-z]{4,10}$/i.test(userId.name);
+            const passwordValidation = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,10}$/i.test(userId.password);
+            try {
+                state.user = userId
+                if(!userValidation || !passwordValidation){
+                    state.user.authUser = false
+                } else {
+                    state.user.authUser = true
+                    const saveState = JSON.stringify(userId)
+                    sessionStorage.setItem("authUser", saveState)
+                }
+            } catch (error) {
+                return error
+            }
        },
        logout:(state,action)=> {
 
