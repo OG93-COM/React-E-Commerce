e9c0d19a2bab99592efd97d2c76819b3d4db8a11
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-08-20 00:56:19 +0100

    ShopOG

--- ShopOG/src/assets/data.js
+++ ShopOG/src/assets/data.js
@@ -1,76 +1,71 @@
-import prodcut1 from '../assets/productIMG/product1.jpg'
-import prodcut2 from '../assets/productIMG/product2.jpg'
-import prodcut3 from '../assets/productIMG/product3.jpg'
-import prodcut4 from '../assets/productIMG/product4.jpg'
-import prodcut5 from '../assets/productIMG/product5.jpg'
-
-
-
-
+import prodcut1 from "../assets/productIMG/product1.jpg";
+import prodcut2 from "../assets/productIMG/product2.jpg";
+import prodcut3 from "../assets/productIMG/product3.jpg";
+import prodcut4 from "../assets/productIMG/product4.jpg";
+import prodcut5 from "../assets/productIMG/product5.jpg";
 
 export const storeData = [
-    {
-      id: "1",
-      img: prodcut1,
-      name: "C4 Carbon",
-      text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-      type: "Fins",
-      size: ["39-41", "41-43", "43-45"],
-      price: 345,
-    },
-    {
-      id: "2",
-      img: prodcut2,
-      name: "Beuchat Carbon Monadial",
-      text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-      type: "Fins",
-      size: ["39-41", "41-43", "43-45"],
-      price: 255,
-    },
-    {
-      id: "3",
-      img: prodcut3,
-      name: "Cressi Glass",
-      text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-      type: "Masks",
-      size: [],
-      price: 75,
-    },
-    {
-      id: "4",
-      img: prodcut4,
-      name: "F8 2019",
-      text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-      type: "Flashlight",
-      size: [],
-      price: 105,
-    },
-    {
-      id: "5",
-      img: prodcut5,
-      name: "Hypselon Miroir",
-      text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-      type: "Masks",
-      size: [],
-      price: 49,
-    },
-    {
-        id: "6",
-        img: prodcut2,
-        name: "Beuchat Carbon Monadial",
-        text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-        type: "Fins",
-        size: ["M", "L", "XL"],
-        price: 55,
-      },
-    {
-        id: "7",
-        img: prodcut5,
-        name: "Hypselon Miroir",
-        text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
-        type: "Masks",
-        size: ["M", "L", "XL"],
-        price: 15,
-      },
-
-  ];
+  {
+    id: "1",
+    img: prodcut1,
+    name: "C4 Carbon",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Fins",
+    size: ["39-41", "41-43", "43-45"],
+    price: 345,
+  },
+  {
+    id: "2",
+    img: prodcut2,
+    name: "Beuchat Carbon Monadial",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Fins",
+    size: ["39-41", "41-43", "43-45"],
+    price: 255,
+  },
+  {
+    id: "3",
+    img: prodcut3,
+    name: "Cressi Glass",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Masks",
+    size: [],
+    price: 75,
+  },
+  {
+    id: "4",
+    img: prodcut4,
+    name: "F8 2019",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Flashlight",
+    size: [],
+    price: 105,
+  },
+  {
+    id: "5",
+    img: prodcut5,
+    name: "Hypselon Miroir",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Masks",
+    size: [],
+    price: 49,
+  },
+  {
+    id: "6",
+    img: prodcut2,
+    name: "Beuchat Carbon Monadial",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Fins",
+    size: ["M", "L", "XL"],
+    price: 55,
+  },
+  {
+    id: "7",
+    img: prodcut5,
+    name: "Hypselon Miroir",
+    text: "Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.",
+    type: "Masks",
+    size: ["M", "L", "XL"],
+    price: 15,
+  },
+];
